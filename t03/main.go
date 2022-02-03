1bdac1be1b20398b19d49fea627d6f1cfedb3d1b
김성현 <31617752+iamGreedy@users.noreply.github.com>
2022-02-03 19:11:50 +0900

    약간 코드 작성

--- t03/main.go
+++ t03/main.go
@@ -9,10 +9,12 @@ import (
 func main() {
 	const H = "Hello"
 	const W = "World"
-	//
-	help(H, W)
-	//
-	var vstr = H + ", " + W
+
+	var buf = make([]byte, len(W)+len(H))
+	copy(buf, H)
+	copy(buf[len(H):], W)
+	var vstr = string(buf)
+
 	fmt.Println(vstr)
 	{
 		a := (*reflect.StringHeader)(unsafe.Pointer(&vstr))
@@ -21,13 +23,6 @@ func main() {
 	}
 	fmt.Println(vstr)
 }
+
 func help(a string, b string) {
-	var vstr = a + ", " + b
-	fmt.Println(vstr)
-	{
-		a := (*reflect.StringHeader)(unsafe.Pointer(&vstr))
-		bts := unsafe.Slice((*byte)(unsafe.Pointer(a.Data)), 5)
-		bts[0] = byte('A')
-	}
-	fmt.Println(vstr)
 }
